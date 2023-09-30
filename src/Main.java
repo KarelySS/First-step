8f67c048a51ecd0658853472258fca07d8048f57
79535 <twinm124@gmail.com>
2023-09-30 13:05:29 +0300

    variables

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
         System.out.println("Hello world!!!");
         System.out.println("Hello world!!!");
         System.out.println("Hello world!!!");
+        System.out.println("Hello world!!!");
+        System.out.println("Hello world!!!");
         var a = 1;
         var b = 2;
         var c = 1;
