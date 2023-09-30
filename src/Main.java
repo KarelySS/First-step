0064543bcaa8d7e04d4cefb5d260289a4fdb3719
79535 <twinm124@gmail.com>
2023-09-30 13:03:49 +0300

    variables

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
+        System.out.println("Hello world!!!");
+        System.out.println("Hello world!!!");
+        System.out.println("Hello world!!!");
         var a = 1;
         var b = 2;
         var c = 2;
